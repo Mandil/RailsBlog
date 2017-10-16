108920e31f198d4d69e968738151b56b63d0571f
mandil <hassan.almandeel@gmail.com>
2017-10-15 20:02:10 -0700

    editing auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  http_basic_authenticate_with name: 'mandil', password: 'password', except: [:index, :show]
-  
+  http_basic_authenticate_with name: 'mandil', password: 'password', except: [:index, :show, :new]
+
   def index
     @posts = Post.all
   end
